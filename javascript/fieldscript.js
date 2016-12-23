fe768fd19aedd063b6ed0b61d5843a05c0e63102
Lancelot Blanchard <lancelotblanchard@hotmail.fr>
2016-12-23 21:42:28 +0100

    Communication with heroku PHP App works

--- javascript/fieldscript.js
+++ javascript/fieldscript.js
@@ -2,7 +2,7 @@ $(function(){
 	//var sampleTags = ['Apple', 'Orange', 'c++', 'java', 'php', 'coldfusion', 'javascript', 'asp', 'ruby', 'python', 'c', 'scala', 'groovy', 'haskell', 'perl', 'erlang', 'apl', 'cobol', 'go', 'lua'];
 	var sampleTagsFrench = ['Joie', 'Colère', 'Haine', 'Rage', 'Tristesse', 'Peur', 'Tranquillité', 'Soulagement', 'Interrogation', 'Angoisse', 'Peine', 'Dégoût', 'Stress', 'Frustration', 'Tension', 'Dérangement', 'Fureur', 'Exaspération', 'Affolement',
 	'Excitation', 'Alerte', 'Confusion', 'Réticence', 'Désarroi', 'Détresse', 'Déprime', 'Effroi', 'Épouvante', 'Désespoir', 'Méfiance', 'Ennui', 'Perplexité', 'Épuisement', 'Résignation' ,'Perturbation', 'Impuissance', 'Solitude', 'Pression', 'Souffrance',
-	'Gêne', 'Lassitude', 'Surprise', 'Terreur'];
+	'Gêne', 'Lassitude', 'Surprise', 'Terreur', 'Calme'];
 
 	//-------------------------------
 	// Single field
